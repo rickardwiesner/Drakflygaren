de99d3d9821b6606aecbc3ba3ca5fa8452dabe4c
nitin anand <nitinanand27@yahoo.com>
2016-09-12 14:45:32 +0200

    lets start

---
+++ Drakflygaren/Drakflygaren/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Drakflygaren.Startup))]
+namespace Drakflygaren
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
