ad6e387f57db9eb40e1af1adad5924c51ad1a1bf
Calle Sandstrom <callesandstrom@hotmail.com>
2016-09-12 15:18:14 +0200

    *Test

--- Drakflygaren/Drakflygaren/Controllers/PlacesController.cs
+++ Drakflygaren/Drakflygaren/Controllers/PlacesController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Drakflygaren.Controllers
 {
     public class PlacesController
     {
-        
+        public ActionResult Index()
+        {
+            return View();
+        } 
     }
 }
