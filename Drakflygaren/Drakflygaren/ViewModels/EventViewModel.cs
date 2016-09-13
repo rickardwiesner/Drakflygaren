1e4ab954e2be42c9edd8b33e470a357a9d7546b3
Rickard Wiesner <rickard.wiesner@hotmail.com>
2016-09-13 23:06:55 +0200

    Created EventController/View

---
+++ Drakflygaren/Drakflygaren/ViewModels/EventViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Drakflygaren.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Drakflygaren.ViewModels
+{
+    public class EventViewModel
+    {
+        public Event Event { get; set; }
+        [Required]
+        public string EventName { get; set; }
+        [Required]
+        public DateTime EventDateTime { get; set; }
+        public string EventImageUrl { get; set; }
+        [Required]
+        public string EventDesc { get; set; }
+        [Required]
+        public int EventLocationId { get; set; }
+        public bool Attending { get; set; }
+        public bool Liked { get; set; }
+    }
+}
