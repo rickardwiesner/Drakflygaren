6141758752f4bd2dab0d2e44b23bf1dd298bfbe8
nitin anand <nitinanand27@yahoo.com>
2016-09-16 14:54:59 +0200

    VädretAPI

---
+++ Drakflygaren/Drakflygaren/Scripts/weather.js
@@ -0,0 +1,30 @@
+﻿//Reads each place from table in Index View & returns its respective data
+
+//function that takes a location & on success fills requested data
+function loadWeather(location) {
+    $.simpleWeather({
+        location: location,
+        unit: 'c',
+        success: function (weather) {
+            $('.cityTemp').html(weather.temp);
+            $('.cityWind').html(weather.wind.speed);
+            $('.direction').html(weather.wind.direction);
+        },
+        error: function (error) {
+            $("#cityTemp").html('<p>' + error + '</p>');
+        }
+    });
+}
+
+//loading all cities from foreach in index view in a list
+var list = document.getElementsByClassName('cityName');
+
+//iterating all cities one by one to load their data
+for (var i = 0; i < list.length; i++)
+{
+    loadWeather(list[i].innerHTML);
+}
+
+
+
+
