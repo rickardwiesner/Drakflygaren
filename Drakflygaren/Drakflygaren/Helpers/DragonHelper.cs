14b62700a71bc3392e53b8505312d2a9a692dfb8
Calle Sandstrom <callesandstrom@hotmail.com>
2016-09-26 12:32:40 +0200

    *datepicker *dragonhelper

---
+++ Drakflygaren/Drakflygaren/Helpers/DragonHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Drakflygaren.Helpers
+{
+    public class DragonHelper
+    {
+        static public string GetTimeSpan(DateTime dateTimeFinished)
+        {
+            if ((DateTime.Now.Subtract(dateTimeFinished.TimeOfDay).Hour < 1) && (DateTime.Now.Subtract(dateTimeFinished.Date).Days < 1))
+            {
+                return string.Format("{0}{1}", DateTime.Now.Subtract(dateTimeFinished.TimeOfDay).Minute, " minuter sedan");
+            }
+
+            else if ((DateTime.Now.Subtract(dateTimeFinished.TimeOfDay).Hour < 24) && (DateTime.Now.Subtract(dateTimeFinished.Date).Days < 1))
+            {
+                return string.Format("{0}{1}", DateTime.Now.Subtract(dateTimeFinished.TimeOfDay).Hour, " timmar sedan");
+
+            }
+
+            else if (DateTime.Now.Subtract(dateTimeFinished.Date).Days < 7)
+            {
+                return string.Format("{0}{1}", DateTime.Now.Subtract(dateTimeFinished.Date).Days, " dagar sedan");
+            }
+
+            else
+            {
+                return string.Format("{0}{1}", DateTime.Now.Subtract(dateTimeFinished.Date).Days / 7, " veckor sedan");
+            }
+        }
+    }
+}
