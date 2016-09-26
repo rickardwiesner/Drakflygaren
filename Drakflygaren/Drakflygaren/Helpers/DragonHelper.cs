f3e0119b30480c7c3d36a354cc128087066d7651
Calle Sandstrom <callesandstrom@hotmail.com>
2016-09-26 12:52:41 +0200

    *mini

--- Drakflygaren/Drakflygaren/Helpers/DragonHelper.cs
+++ Drakflygaren/Drakflygaren/Helpers/DragonHelper.cs
@@ -11,23 +11,23 @@ namespace Drakflygaren.Helpers
         {
             if ((DateTime.Now.Subtract(lastPost.TimeOfDay).Hour < 1) && (DateTime.Now.Subtract(lastPost.Date).Days < 1))
             {
-                return string.Format("{0}{1}", DateTime.Now.Subtract(lastPost.TimeOfDay).Minute, " m sedan");
+                return string.Format("{0}{1}", DateTime.Now.Subtract(lastPost.TimeOfDay).Minute, "m sedan");
             }
 
             else if ((DateTime.Now.Subtract(lastPost.TimeOfDay).Hour < 24) && (DateTime.Now.Subtract(lastPost.Date).Days < 1))
             {
-                return string.Format("{0}{1}", DateTime.Now.Subtract(lastPost.TimeOfDay).Hour, " h sedan");
+                return string.Format("{0}{1}", DateTime.Now.Subtract(lastPost.TimeOfDay).Hour, "h sedan");
 
             }
 
             else if (DateTime.Now.Subtract(lastPost.Date).Days < 7)
             {
-                return string.Format("{0}{1}", DateTime.Now.Subtract(lastPost.Date).Days, " d sedan");
+                return string.Format("{0}{1}", DateTime.Now.Subtract(lastPost.Date).Days, "d sedan");
             }
 
             else
             {
-                return string.Format("{0}{1}", DateTime.Now.Subtract(lastPost.Date).Days / 7, " v sedan");
+                return string.Format("{0}{1}", DateTime.Now.Subtract(lastPost.Date).Days / 7, "v sedan");
             }
         }
     }
